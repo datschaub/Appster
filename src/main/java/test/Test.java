0185a1fe77021d8f77f178f49fd451ebe4c33a7d
FredrikLindahl <lindahl.f@gmail.com>
2016-01-22 10:16:41 +0100

    asdsa
    asd

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -4,5 +4,10 @@ public class Test {
 
 	public String name;
 	public int age;
+	public int height;
+	public int weight;
 	
+	public int getBMI(int weight, int height){
+		return weight/height;
+	}
 }
